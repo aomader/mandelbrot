54bf734e50d24c6df0730c70ec5fa6f9403fec8d
Oliver Mader <b52@reaktor42.de>
2013-12-14 17:12:07 +0100

    Add simple Swing JFrame

---
+++ src/mandelbrot/Main.java
@@ -0,0 +1,16 @@
+package mandelbrot;
+
+import mandelbrot.ui.MainWindow;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainWindow();
+            }
+        });
+    }
+}
---
+++ src/mandelbrot/ui/MainWindow.java
@@ -0,0 +1,12 @@
+package mandelbrot.ui;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+        setTitle("Mandelbrot");
+        setSize(200, 200);
+        setVisible(true);
+    }
+}
